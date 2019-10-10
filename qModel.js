477dd712e97527d05ae0cadcedc525101282f41a
Pooja Goyal <poojagl85>
2019-10-10 17:16:23 +0530

    second

---
+++ qModel.js
@@ -0,0 +1,1 @@
+const mongoose=require("mongoose");mongoose.connect("mongodb://localhost:27017/DazzlingWheels",{useNewUrlParser:!0,useUnifiedTopology:!0});const Schema=mongoose.Schema,querySchema=new Schema({custfName:String,custlName:String,custNumb:Number,custEmail:String,messAge:String});module.exports=mongoose.model("query",querySchema);
